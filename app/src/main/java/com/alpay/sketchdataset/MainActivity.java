3b5fa95c39ccd050c20e1f243a334834e0e72e83
alpay sabuncuoglu <asabuncuoglu13@ku.edu.tr>
2018-10-17 00:07:37 +0300

    init-sketch-dataset

---
+++ app/src/main/java/com/alpay/sketchdataset/MainActivity.java
@@ -0,0 +1,105 @@
+package com.alpay.sketchdataset;
+
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.os.Environment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.alpay.sketchdataset.view.InkView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    InkView inkView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inkView = (InkView) findViewById(R.id.ink);
+        inkView.hasFlag(InkView.FLAG_INTERPOLATION);
+        inkView.addFlag(InkView.FLAG_RESPONSIVE_WIDTH);
+        inkView.setBackgroundColor(Color.WHITE);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setDialogPopUp(R.string.app_name);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_clear:
+                inkView.clear();
+                return true;
+            case R.id.menu_draw:
+                inkView.setColor(Color.BLACK);
+                return true;
+            case R.id.menu_erase:
+                inkView.setColor(Color.WHITE);
+                return true;
+            case R.id.menu_send:
+                setDialogPopUp(R.string.app_name);
+                saveBitmap();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveBitmap(){
+        Bitmap bitmap = inkView.getBitmap();
+        Bitmap bmp = inkView.getBitmap();
+        Canvas canvas = new Canvas();
+        canvas.setBitmap(bitmap);
+        canvas.drawColor(Color.WHITE);
+        canvas.drawBitmap(bmp, 0, 0, null);
+        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                "/PhysicsSketchpad";
+        File dir = new File(file_path);
+        if(!dir.exists())
+            dir.mkdirs();
+        File file = new File(dir, "sketchpad" + System.currentTimeMillis() + ".png");
+        try{
+            FileOutputStream fOut = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        inkView.clear();
+    }
+
+    private void setDialogPopUp(int id){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getResources().getText(id))
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
